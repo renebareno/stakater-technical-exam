0d35aa74867c42938182557eacdb06d4556cc3da
Rene Arturo Bareño Campos <rene.bareno@gmail.com>
2021-06-11 00:08:41 -0500

    Initial commit backend, with spring boot

---
+++ src/main/java/com/stakater/technical/exam/StakaterTechnicalExamApplication.java
@@ -0,0 +1,13 @@
+package com.stakater.technical.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StakaterTechnicalExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StakaterTechnicalExamApplication.class, args);
+	}
+
+}
